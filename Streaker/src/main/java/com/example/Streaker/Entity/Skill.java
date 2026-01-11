cd95915973e5725432c8796326c08ad738524e17
Mohan Murali <miriyalamohanmurali74@gmail.com>
2026-01-11 17:35:29 +0530

    written comments for better clarity of code in Skill entity

--- Streaker/src/main/java/com/example/Streaker/Entity/Skill.java
+++ Streaker/src/main/java/com/example/Streaker/Entity/Skill.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="skills")
-@Data
+@Data     // Lombok is implemented instead of getters and setters
 @AllArgsConstructor
 @NoArgsConstructor
 public class Skill {
@@ -15,16 +15,16 @@ public class Skill {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    // Here we used nullable to make the user not to enter the accurate data and not to keep empty
     @Column(nullable = false, unique = true)
     private String name;
-
+    //Here the category is like ex: java,DSA,JS...etc. whatever you want do
     @Column(nullable = false)
     private String category;
-
+    //Here this is for to indicate no of unproductive days.
     @Column(nullable = false)
     private int decayDays;
-
+    // this is for status of activeness
     @Column(nullable = false)
     private boolean active = true;
 }
