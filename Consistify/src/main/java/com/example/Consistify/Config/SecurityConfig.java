43c08e028f044c3101baeff3f4abb7b8a1f8aaa8
Mohan Murali <miriyalamohanmurali74@gmail.com>
2026-02-17 21:07:53 +0530

    Changes in application properties and the security config

--- Consistify/src/main/java/com/example/Consistify/Config/SecurityConfig.java
+++ Consistify/src/main/java/com/example/Consistify/Config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
     private final CustomOAuth2UserService customOAuth2UserService;
 
     // Pull frontend URL from environment variable
-    @Value("${app.frontend.url:https://consistify-six.vercel.app/}")
+    @Value("${app.frontend.url:https://consistify-six.vercel.app}")
     private String frontendUrl;
     @Bean
     public PasswordEncoder passwordEncoder() {
