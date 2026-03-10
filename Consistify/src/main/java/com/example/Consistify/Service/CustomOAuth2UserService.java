e4303f41ee67a7d6b445b67cea045b8182fa9fa2
Mohan Murali <miriyalamohanmurali74@gmail.com>
2026-03-10 19:12:04 +0530

    made updates in customauth

--- Consistify/src/main/java/com/example/Consistify/Service/CustomOAuth2UserService.java
+++ Consistify/src/main/java/com/example/Consistify/Service/CustomOAuth2UserService.java
@@ -24,27 +24,18 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         String email = oAuth2User.getAttribute("email");
         String name = oAuth2User.getAttribute("name");
 
-        System.out.println("GOOGLE LOGIN EMAIL: " + email);
-
-        if (email == null) {
-            throw new RuntimeException("Email not received from Google OAuth");
-        }
-
-        email = email.trim().toLowerCase();
-
+        // Fetch or create user
         User user = userRepository.findByEmail(email)
                 .orElseGet(() -> {
-                    System.out.println("Creating new user: " + email);
-
                     User newUser = User.builder()
                             .email(email)
                             .name(name)
                             .role(Role.USER)
                             .password(null)
                             .build();
-
                     return userRepository.save(newUser);
                 });
 
         return oAuth2User;
     }
+}
